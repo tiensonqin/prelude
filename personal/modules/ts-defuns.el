4a6b74c0c0f7828d57f9c5d1b4fed0560bf80e2a
Tienson Qin <tiensonqin@gmail.com>
2019-10-05 06:34:30 +0800

    Add update-env

--- personal/modules/ts-defuns.el
+++ personal/modules/ts-defuns.el
@@ -157,4 +157,19 @@ In the shell command, the file(s) will be substituted wherever a '%' is."
     (setq comint-buffer-maximum-size old-max)
     (goto-char (point-max))))
 
+(defun my-update-env ()
+  (interactive)
+  (let ((str
+         (with-temp-buffer
+           (insert-file-contents "/tmp/env.txt")
+           (buffer-string))) lst)
+    (setq lst (split-string str "\000"))
+    (while lst
+      (setq cur (car lst))
+      (when (string-match "^\\(.*?\\)=\\(.*\\)" cur)
+        (setq var (match-string 1 cur))
+        (setq value (match-string 2 cur))
+        (setenv var value))
+      (setq lst (cdr lst)))))
+
 (provide 'ts-defuns)
--- personal/modules/ts-elixir.el
+++ personal/modules/ts-elixir.el
@@ -5,27 +5,28 @@
 ;;   (ruby-end-mode +1))
 ;; (add-hook 'elixir-mode-hook 'auto-activate-ruby-end-mode-for-elixir-mode)
 
-(require 'alchemist)
-(add-hook
- 'alchemist-mode-hook
- (lambda ()
-   (define-key alchemist-mode-map (kbd "\C-c\C-h") 'alchemist-help-search-at-point)
-   (define-key alchemist-mode-map (kbd "\C-c n") 'elixir-format)
-   ))
-(setq alchemist-help-ansi-color t)
+;; (require 'alchemist)
+;; (add-hook
+;;  'alchemist-mode-hook
+;;  (lambda ()
+;;    (define-key alchemist-mode-map (kbd "\C-c\C-h") 'alchemist-help-search-at-point)
+;;    (define-key alchemist-mode-map (kbd "\C-c n") 'elixir-format)
+;;    ))
+;; (setq alchemist-help-ansi-color t)
 
 ;; (add-hook 'elixir-mode-hook
 ;;           (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 
-;; (add-hook 'elixir-mode-hook #'lsp)
+(add-hook 'elixir-mode-hook #'lsp)
 
 ;; (lsp-register-client
 ;;  (make-lsp-client :new-connection (lsp-stdio-connection '("~/codes/source/elixir/elixir-ls/language_server.sh"))
 ;;                   :major-modes '(elixir-mode)
 ;;                   :server-id 'elixir-ls
-;;                   :use-native-json t))
+;;                   ))
 
 
 ;; enable elixir-mix
 ;; (global-elixir-mix-mode)
+(setq lsp-ui-doc-delay 100)
 (provide 'ts-elixir)
